8e9d4393b4702c0df784b33ad7997ff887df8ea9
emily <775363160@qq.com>
2019-01-21 23:39:01 +0800

    day31-二叉树构建前后序

--- 3-chapter3/2-根据前序和后序遍历构造二叉树889.js
+++ 3-chapter3/2-根据前序和后序遍历构造二叉树889.js
@@ -25,7 +25,24 @@ pre 和 post 遍历中的值是不同的正整数。
  * 4   5           4
  *                / \
  *                   5
+ * 递归思想：
+ *      我们可以在前序里确定根元素
+ *      然后在区分前序的时候：试探？
+ * ==-> 前序的第二个元素可以确定的是 属于前序
+ *      那么我们可以在后序里找到该元素的位置，依据左右根
+ *      知道后序中，至少在该元素以前的元素都是左区域的
+ *      那么可以依据此划分左右。
+ * btw: slice(start,end)是左闭右开
  */
 function constructFromPrePost(pre,post){
-
+    if(pre.length<1) return null;
+    if(pre.length==1) return new TreeNode(pre[0]);
+    let head = new TreeNode(pre[0]);
+    let 
+        leftVal = pre[1],
+        leftValIndex =  post.indexOf(leftVal);
+    //+2是因为 1左闭右开的原因，2.去除掉第一个元素root
+    head.left = constructFromPrePost(pre.slice(1,leftValIndex+2),post.slice(0,leftValIndex+1));
+    head.right = constructFromPrePost(pre.slice(leftValIndex+2),post.slice(leftValIndex+1));
+    return head;
 }
